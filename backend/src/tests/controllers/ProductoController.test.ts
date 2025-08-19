0e0b2a103803ef054273b63d23234b9851bee924
JhordyMarcillo <jhordypaulsb2@gmail.com>
2025-08-18 20:01:21 -0500

    Update Proyecto

--- backend/src/tests/controllers/ProductoController.test.ts
+++ backend/src/tests/controllers/ProductoController.test.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import * as { validationResult } from 'express-validator';
 import { ProductoController } from '../../controllers/ProductoController';
 import { ProductoModel } from '../../models/Producto';
 import { CambioModel } from '../../models/Cambio';
 import { Producto, AuthPayload } from '../../types';
+import { validationResult } from 'express-validator/lib/validation-result';
 
 // Mock dependencies
 jest.mock('../../models/Producto');
