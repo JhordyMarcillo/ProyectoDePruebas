36b6837d3b6bbf63907fdef9955926fcdec14676
JhordyMarcillo <jhordypaulsb2@gmail.com>
2025-08-20 23:04:32 -0500

    Update Proyecto

--- backend/k6-tests/sustained-load-test.js
+++ backend/k6-tests/sustained-load-test.js
@@ -10,12 +10,12 @@ export let options = {
         { duration: '15s', target: 0 },
     ],
     thresholds: {
-        http_req_duration: ['p(95)<1000'],
+        http_req_duration: ['p(95)<2000'], 
+        'http_req_duration{endpoint:read}': ['p(95)<1500'],
+        'http_req_duration{endpoint:write}': ['p(95)<2500'],
         http_req_failed: ['rate<0.08'],
         http_reqs: ['rate>150'],
         vus: ['value<=250'],
-        'http_req_duration{endpoint:read}': ['p(95)<1000'],
-        'http_req_duration{endpoint:write}': ['p(95)<1500'],
     }
 };
 
